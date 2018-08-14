3ac064e0d546c7cb0f506ccb86b4eb4766bae014
dengl11 <dengl11@stanford.edu>
2018-08-14 16:24:22 -0700

    graph & mongol good

--- imports/db/collections.js
+++ imports/db/collections.js
@@ -2,4 +2,5 @@ import { Meteor } from 'meteor/meteor'
 
 export const Stdout = new Mongo.Collection('stdout');
 export const TopicCluster = new Mongo.Collection('topicCluster');
+export const DayRatio = new Mongo.Collection('dayRatio');
 
--- server/init/init.js
+++ server/init/init.js
@@ -1,17 +1,34 @@
 import { Meteor } from 'meteor/meteor';
 import { TopicCluster } from '/imports/db/collections.js';
+import { DayRatio } from '/imports/db/collections.js';
 
 Meteor.startup(()=>{
     init_db();
 });
 
 init_db = ()=>{
-    var topic_clusters = JSON.parse(Assets.getText("100_clustering.json"));
+    init_cluster();
+    init_day_ratio();
+};
+
+
+init_cluster = () => {
+    var topic_clusters = JSON.parse(Assets.getText("10000_clustering.json"));
     TopicCluster.remove({});
     for (var key in topic_clusters) {
         if (topic_clusters.hasOwnProperty(key)) {
             TopicCluster.insert({"Topic": key, "Neighbors": topic_clusters[key]});
         }
-    }
+    } 
+}
 
-};
+
+init_day_ratio = () => {
+    var day_ratios = JSON.parse(Assets.getText("10000_topics_day_ratio_by_date.json"));
+    DayRatio.remove({});
+    for (var key in day_ratios) {
+        if (day_ratios.hasOwnProperty(key)) {
+            DayRatio.insert({"Topic": key, "DailyRatio": day_ratios[key]});
+        }
+    } 
+}
