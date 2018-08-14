6ef6ab4e9db491ee03ab1baf102ac2666fa47b05
dengl11 <dengl11@stanford.edu>
2018-08-14 12:39:19 -0700

    fancy box

--- imports/ui/overview/overview.jsx
+++ imports/ui/overview/overview.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
+
+
 export default class Overview extends React.Component {
     render() {
         return (
             <div>
+          <a href="https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201606271147" data-fancybox="images" data-caption="My caption">
+	<img src="https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201606271147" alt="" />
+</a>
             </div>
         );
     }
